a78cff6069859955e6254223787e7cbf017bac76
moezh <moez.hachicha@gmail.com>
2023-01-15 11:51:20 +0000

    0.1.0

--- frontend/src/components/layout.tsx
+++ frontend/src/components/layout.tsx
@@ -12,6 +12,27 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title>Web Application</title>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Head>
       <div className="sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg mx-auto pl-2 pr-2">
         <header className="flex flex-col items-center justify-center pt-4 pb-4">
