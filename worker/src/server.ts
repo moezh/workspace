58db84918267f3bad4ce3e21b5c1229b53899c23
moezh <moez.hachicha@gmail.com>
2023-02-09 01:13:57 +0100

    0.1.0

--- worker/src/server.ts
+++ worker/src/server.ts
@@ -4,8 +4,8 @@ import everyHour from "./everyHour";
 import everyDay from "./everyDay";
 
 (async () => {
-  //await everyHour();
-  //await everyDay();
+  await everyHour();
+  await everyDay();
   new CronJob("* * * * 0", everyHour, null, true);
   new CronJob("* * * 5 0", everyDay, null, true);
 })();
