80c79279d8f01bc6289e98602b5c62a6cc316945
moezh <moez.hachicha@gmail.com>
2023-02-11 22:37:15 +0000

    0.1.0

--- worker/src/services/rss.ts
+++ worker/src/services/rss.ts
@@ -2,13 +2,18 @@ import Parser from "rss-parser";
 
 const parser: Parser = new Parser();
 
-export const getRSS = async (url: string, after?: number) => {
+export const getRSS = async (url: string, minutes?: number) => {
   const rss = await parser.parseURL(url);
-  if (after === undefined) {
+  if (minutes === undefined) {
     return rss.items;
   } else {
+    const rssDate =
+      rss.description === undefined
+        ? new Date()
+        : new Date(rss.description?.replace("All jobs as of ", ""));
+    const startingDate = rssDate.getTime() - minutes * 60 * 1000;
     return rss.items.filter(
-      (item) => new Date(item.isoDate as string).getTime() > after
+      (item) => new Date(item.isoDate as string).getTime() > startingDate
     );
   }
 };
