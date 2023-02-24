a690c5b1d87bf0831851ae13565f77cde4854086
moezh <moez.hachicha@icloud.com>
2023-02-24 16:50:38 +0000

    0.1.0

--- frontend/src/pages/workout/programs/[id].tsx
+++ frontend/src/pages/workout/programs/[id].tsx
@@ -18,15 +18,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   });
   const configData = await config.json();
-  const exercise = await fetch(`http://backend:3001/api/workout/exercises/${id}`, {
+  const program = await fetch(`http://backend:3001/api/workout/programs/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt.sign("admin", password)}`,
       "Content-Type": "application/json",
     },
   });
-  const exerciseData = await exercise.json();
-  if (exerciseData.code === 404) return { notFound: true };
-  return { props: { config: configData, data: exerciseData } };
+  const programData = await program.json();
+  if (programData.code === 404) return { notFound: true };
+  return { props: { config: configData, data: programData } };
 };
 
 export default function Page(props: { data: Record<string, string> }) {
--- frontend/src/pages/workout/workouts/[id].tsx
+++ frontend/src/pages/workout/workouts/[id].tsx
@@ -18,15 +18,15 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     },
   });
   const configData = await config.json();
-  const exercise = await fetch(`http://backend:3001/api/workout/exercises/${id}`, {
+  const workout = await fetch(`http://backend:3001/api/workout/workouts/${id}`, {
     headers: {
       Authorization: `Bearer ${jwt.sign("admin", password)}`,
       "Content-Type": "application/json",
     },
   });
-  const exerciseData = await exercise.json();
-  if (exerciseData.code === 404) return { notFound: true };
-  return { props: { config: configData, data: exerciseData } };
+  const workoutData = await workout.json();
+  if (workoutData.code === 404) return { notFound: true };
+  return { props: { config: configData, data: workoutData } };
 };
 
 export default function Page(props: { data: Record<string, string> }) {
